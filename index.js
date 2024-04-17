c3f6e4f8d586a68144beba05f06ee843b8c5e948
RukhsarMalik <103738665+RukhsarMalik@users.noreply.github.com>
2024-04-17 23:18:20 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a right number between 1 to 6: "
+    }]);
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! You have gueesed a right number ");
+}
+else
+    (console.log("Better luck next time."));
